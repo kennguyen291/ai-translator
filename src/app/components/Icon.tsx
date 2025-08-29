b9b0bc202636cfe818e1a4d2cfc63fa6d51b8275
kennguyen291 <khangnguyen291@gmail.com>
2025-08-29 13:46:17 +1000

    move component to seperate folders

---
+++ src/app/components/Icon.tsx
@@ -0,0 +1,19 @@
+import React, { FC } from "react";
+
+interface IconProps {
+  path: string;
+  className?: string;
+}
+
+const Icon: FC<IconProps> = ({ path, className = "w-6 h-6" }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    fill="currentColor"
+    className={className}
+  >
+    <path d={path} />
+  </svg>
+);
+
+export default Icon;
---
+++ src/app/components/Spinner.tsx
@@ -0,0 +1,7 @@
+import React, { FC } from "react";
+
+const Spinner: FC = () => (
+  <div className="w-5 h-5 border-2 border-t-transparent border-white rounded-full animate-spin"></div>
+);
+
+export default Spinner;
